ba45db21bfaa9a4707384f29d73e9c854e3a6775
xeian_sangeet@hotmail.com <xeian_sangeet@hotmail.com>
2026-04-05 14:32:31 +0200

    added noodesl

---
+++ config.js
@@ -0,0 +1,9 @@
+// SpicyNoodles Configuration
+// CUSTOMIZE: Update contact information here
+
+const config = {
+  email: 'niru.nirajanpokharel@gmail.com',
+  whatsapp: 'https://wa.me/', // Add your WhatsApp number here
+  website: 'https://www.spicynoodles.nl/',
+  location: 'Noodels from the footsteps of Himalayas'
+};
